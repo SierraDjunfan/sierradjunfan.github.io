342789b4392c16309ab26925b053100659f1d8e5
sierradjunfan <sara_byrne@live.com.au>
2019-07-07 21:16:17 +1000

    Make a landing page. Put minesweeper in a /minesweeper directory.

---
+++ minesweeper/precache-manifest.045c1408122ad27597d561df08e7ccc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2792fe8866bbb1bf34d35738e086dc63",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "e8fc82a35f0dd6d0e045",
+    "url": "/minesweeper/static/css/main.5ef85204.chunk.css"
+  },
+  {
+    "revision": "6539ca0063d1eb508a0b",
+    "url": "/minesweeper/static/js/2.41c59357.chunk.js"
+  },
+  {
+    "revision": "e8fc82a35f0dd6d0e045",
+    "url": "/minesweeper/static/js/main.24f141cd.chunk.js"
+  },
+  {
+    "revision": "0f86e2ffe97a36d3984a",
+    "url": "/minesweeper/static/js/runtime~main.dd26f679.js"
+  },
+  {
+    "revision": "527343902fe176221dd54a352213cfb2",
+    "url": "/minesweeper/static/media/lost.52734390.png"
+  },
+  {
+    "revision": "b92770c5cc3133b0dc436701188c2fa2",
+    "url": "/minesweeper/static/media/mine.b92770c5.png"
+  },
+  {
+    "revision": "02b8c4905267e8a4b251902387558275",
+    "url": "/minesweeper/static/media/normal.02b8c490.png"
+  }
+]);
